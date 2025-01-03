ec0d95257f94dd08a4f45c1347cb635cf214598e
Manibingi <bingimaniprasad01@gmail.com>
2025-01-03 23:44:51 +0530

    four

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,8 +1,8 @@
-const URL = "http://localhost:8000/api";
+// const URL = "http://localhost:8000/api";
 const apiUrl = import.meta.env.VITE_API_URI;
 
 export const register = (data) => {
-  return fetch(`${apiUrl}/auth/register`, {
+  return fetch(`${apiUrl}/api/auth/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -12,7 +12,7 @@ export const register = (data) => {
 };
 
 export const login = (data) => {
-  return fetch(`${apiUrl}/auth/login`, {
+  return fetch(`${apiUrl}/api/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
